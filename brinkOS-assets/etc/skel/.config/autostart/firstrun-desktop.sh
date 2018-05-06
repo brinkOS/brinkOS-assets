3b30c83d3c0a5f13d4a32f1e71d3054864e20d28
James Brink <brink.james@gmail.com>
2018-05-06 13:48:40 -0700

    Added cinnamon panel config

--- brinkOS-assets/etc/skel/.config/autostart/firstrun-desktop.sh
+++ brinkOS-assets/etc/skel/.config/autostart/firstrun-desktop.sh
@@ -6,6 +6,11 @@ gsettings set org.cinnamon.theme name "brinkOS-Dark"
 gsettings set org.cinnamon.desktop.interface icon-theme "brinkOS"
 gsettings set org.cinnamon.desktop.wm.preferences button-layout 'menu:minimize,maximize,close'
 gsettings set org.cinnamon.settings-daemon.plugins.xsettings overrides "{'Gtk/DecorationLayout': <':minimize,maximize,close'>}"
+gsettings set org.nemo.desktop show-desktop-icons true
+gsettings set org.nemo.desktop computer-icon-visible true
+gsettings set org.nemo.desktop home-icon-visible true
+gsettings set org.nemo.desktop trash-icon-visible true
+
 # gsettings set org.cinnamon favorite-apps "['google-chrome.desktop', 'ice.desktop', 'org.gnome.Nautilus.desktop', 'geary.desktop', 'empathy.desktop', 'gnome-music.desktop', 'org.gnome.Photos.desktop', 'org.gnome.Totem.desktop', 'libreoffice-writer.desktop', 'libreoffice-impress.desktop', 'libreoffice-calc.desktop', 'org.gnome.gedit.desktop', 'gnome-terminal.desktop', 'gnome-tweak-tool.desktop', 'gnome-control-center.desktop', 'pamac-manager.desktop', 'calamares.desktop']"
 gsettings set org.freedesktop.Tracker.Miner.Files index-recursive-directories "['$HOME', '&DOCUMENTS', '&DOWNLOAD', '&MUSIC', '&PICTURES', '&VIDEOS']"
 gsettings set org.freedesktop.Tracker.Miner.Files crawling-interval -2
