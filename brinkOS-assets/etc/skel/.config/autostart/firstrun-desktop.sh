27b15095eb4c8225e54ee75f276e22a54c06b205
James Brink <brink.james@gmail.com>
2018-05-06 15:50:13 -0700

    Added settings for pamac to use AUR

--- brinkOS-assets/etc/skel/.config/autostart/firstrun-desktop.sh
+++ brinkOS-assets/etc/skel/.config/autostart/firstrun-desktop.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click false
 gsettings set org.cinnamon.desktop.interface gtk-theme "brinkOS-Dark"
 gsettings set org.cinnamon.desktop.wm.preferences theme "brinkOS-Dark"
 gsettings set org.cinnamon.theme name "brinkOS-Dark"
@@ -10,14 +10,20 @@ gsettings set org.nemo.desktop show-desktop-icons true
 gsettings set org.nemo.desktop computer-icon-visible true
 gsettings set org.nemo.desktop home-icon-visible true
 gsettings set org.nemo.desktop trash-icon-visible true
-gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
 
+# Terminal settings
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
 profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
 profile=${profile:1:-1} # remove leading and trailing single quotes
 gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" visible-name 'brinkOS'
 gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" use-transparent-background true
 gsettings set "org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profile/" background-transparency-percent 3
 
+
+# Pamac settings
+gsettings set org.pamac.aur enable-aur true
+gsettings set org.pamac.aur check-aur-updates true
+gsettings set org.pamac.aur search-in-aur true
 
 # gsettings set org.cinnamon favorite-apps "['google-chrome.desktop', 'ice.desktop', 'org.gnome.Nautilus.desktop', 'geary.desktop', 'empathy.desktop', 'gnome-music.desktop', 'org.gnome.Photos.desktop', 'org.gnome.Totem.desktop', 'libreoffice-writer.desktop', 'libreoffice-impress.desktop', 'libreoffice-calc.desktop', 'org.gnome.gedit.desktop', 'gnome-terminal.desktop', 'gnome-tweak-tool.desktop', 'gnome-control-center.desktop', 'pamac-manager.desktop', 'calamares.desktop']"
 gsettings set org.freedesktop.Tracker.Miner.Files index-recursive-directories "['$HOME', '&DOCUMENTS', '&DOWNLOAD', '&MUSIC', '&PICTURES', '&VIDEOS']"
